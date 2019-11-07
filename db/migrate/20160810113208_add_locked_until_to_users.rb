29a49758fea81d8ddf04bbe06eefe50242c7e4cc
Stefano Salvador <stefano.salvador@protezionecivile.fvg.it>
2019-11-07 12:16:52 +0100

    Migration version

--- db/migrate/20160810113208_add_locked_until_to_users.rb
+++ db/migrate/20160810113208_add_locked_until_to_users.rb
@@ -1,4 +1,4 @@
-class AddLockedUntilToUsers < ActiveRecord::Migration
+class AddLockedUntilToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :casino_users, :locked_until, :datetime
   end
